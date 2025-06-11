a751225d4f889e322b3fc0fb687a04d10e28d7b4
sonali <sonalijayakodi2002@gmail.com>
2025-06-11 20:51:18 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector("#menu-icon");
+let navmenu = document.querySelector(".nav-menu");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-bars");
+  menu.classList.toggle("fa-times");
+  navmenu.classList.toggle("open");
+};
